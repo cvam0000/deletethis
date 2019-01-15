e2799560faf6f83a3a0f41695497e51a6adb3e4d
cvam0000 <cvam0000@gmail.com>
2019-01-15 20:44:26 +0530

    timeline

--- decompiled_backend/timeline/views.py
+++ decompiled_backend/timeline/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+def index(request):
+    if request.user.is_authenticated:
+        return render(request,'timeline.html')
+    else:
+        return ('url login')
