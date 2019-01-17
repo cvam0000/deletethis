82dec162da41d6a96aa54bb3948233316a57d916
cvam0000 <cvam0000@gmail.com>
2019-01-17 23:10:39 +0530

    jdbjdb

--- decompiled_backend/timeline/views.py
+++ decompiled_backend/timeline/views.py
@@ -22,12 +22,27 @@ def bot(a):
     if a:
         bot = telepot.Bot('438212244:AAGQ1VhzLHowzxUTFiT6GTirIVxjL3PO79I')
         #po = Post.objects.latest('id')
-        po=Post.objects.all()
+        po=Post.objects.last()
         #c=Post.published_date.last()
         #result = [Post.published_date.latest('date_added')]
-        text=po[1]
-        bot.sendMessage(499134543, '1')
-        bot.sendMessage(680139252, '1')
+
+        text='Hi @ all this is from timeline @cronbyte  \n Title: meeting  \n Time: after ILUGD meet  \n\n\n   still need hellozee chat id   '
+        
+        bot.sendMessage(499134543, po )
+        #ajx
+      #  bot.sendMessage(680139252, text)
+        #mason
+      #  bot.sendMessage(660463343, text)
+        #ayan
+      #  bot.sendMessage(400670226, text)
+        #alok
+       # bot.sendMessage(271397625, text)
+        #dunken
+     #   bot.sendMessage(399195869, text)
+        #math
+      #  bot.sendMessage(407034913, text)
+        #bit_bang
+      #  bot.sendMessage(-1001163644641,text)
 
 
 
